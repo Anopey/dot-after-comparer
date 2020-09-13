cdfccf54c83eacd107b8616a1dbd83e38b4f5ca4
Ozan Ürkmez <arkantos250@gmail.com>
2020-09-13 21:29:16 +0300

    change and add basis of first

---
+++ two_files_after_comparer.py
@@ -0,0 +1,53 @@
+checked_file_path = input("ener the path to file you would like checked: ")
+comparison_file_path = input("enter path to base comparison file: ")
+
+checked_file = open(checked_file_path, "r")
+comparison_file = open(comparison_file_path, "r")
+
+checked =  checked_file.read()
+comparison = comparison_file.read()
+
+before = input("what should come before?")
+ending_sequence = input("what should be the character sequence to end it? enter /endofline to indicate until end of line.")
+
+
+if(ending_sequence == "/endofline"):
+    ending_sequence = "\n"
+
+valid = []
+
+comparison_len = len(comparison)
+found = False
+currentFound = ""
+
+for i in range(0, comparison_len):
+    if(comparison[i] == before[0]):
+        found = False
+        for j in range (1, len(before)): #check if before is here
+            if(j + i > comparison_len):
+                break #reached end of file before desired sequence ended.
+            if(comparison[i + j] == ending_sequence[0]): #doing it this way also makes it so that those without ending sequences dont work.
+                ending_sequence_len = len(ending_sequence)
+                for q in range(1, ending_sequence_len):
+                    if (j + i + q > comparison_len):
+                        found = False
+                        break
+                    if comparison[i + j + q] != ending_sequence[q]:
+                        break
+                    if q == ending_sequence_len - 1:
+                        found = True
+                        break
+                if found:
+                    break #Break out of second one.
+
+            if(comparison[i + j] != before[j]):
+                found = False
+                break
+
+            currentFound += comparison[i+j]
+        if found: #found :o
+            valid.append(currentFound)
+        else: #not found :/
+            currentFound = ""
+
+checked_len = len(checked)
